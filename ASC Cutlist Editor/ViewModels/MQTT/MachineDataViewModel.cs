ac2781bf34472370dce603a42c04b472637ff008
Phi <pton@ascmt.com>
2021-08-30 08:09:26 -0700

    Adding mock data source

--- ASC Cutlist Editor/ViewModels/MQTT/MachineDataViewModel.cs
+++ ASC Cutlist Editor/ViewModels/MQTT/MachineDataViewModel.cs
@@ -5,15 +5,22 @@ using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 using AscCutlistEditor.Frameworks;
 using AscCutlistEditor.Models;
+using AscCutlistEditor.Views;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
 using MQTTnet.Server;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace AscCutlistEditor.ViewModels.MQTT
 {
@@ -25,6 +32,10 @@ namespace AscCutlistEditor.ViewModels.MQTT
 
         private ObservableCollection<MachineData> _machineDataCollection = new ObservableCollection<MachineData>();
         private MachineData _latestMachineData = null;
+        private readonly LineSeries _uptimeSeries;
+        private string _connectionStatus;
+
+        public PlotModel PlotModel { get; set; }
 
         public MachineDataViewModel()
         {
@@ -35,6 +46,35 @@ namespace AscCutlistEditor.ViewModels.MQTT
 
             MachineDataCollection = new ObservableCollection<MachineData>();
             LatestMachineData = null;
+            ConnectionStatus = "disconnected";
+
+            // Test plot model
+            // Create the plot model
+            var tmp = new PlotModel { Title = "Uptime", Subtitle = "TESTING" };
+
+            // Create two line series (markers are hidden by default)
+            var series1 = new LineSeries { Title = "Series 1", MarkerType = MarkerType.Square };
+
+            DateTimeAxis xAxis = new DateTimeAxis
+            {
+                Position = AxisPosition.Bottom,
+                StringFormat = "h:mm:ss",
+            };
+
+            var series2 = new LineSeries { Title = "Series 2", MarkerType = MarkerType.Square };
+            series2.Points.Add(new DataPoint(0, 4));
+            series2.Points.Add(new DataPoint(10, 12));
+            series2.Points.Add(new DataPoint(20, 16));
+            series2.Points.Add(new DataPoint(30, 25));
+            series2.Points.Add(new DataPoint(40, 5));
+
+            // Add the series to the plot model
+            tmp.Series.Add(series1);
+            tmp.Axes.Add(xAxis);
+            _uptimeSeries = series1;
+            //tmp.Series.Add(series2);
+
+            PlotModel = tmp;
         }
 
         public ObservableCollection<MachineData> MachineDataCollection
@@ -54,6 +94,16 @@ namespace AscCutlistEditor.ViewModels.MQTT
             {
                 _latestMachineData = value;
                 RaisePropertyChangedEvent("LatestMachineData");
+            }
+        }
+
+        public string ConnectionStatus
+        {
+            get => _connectionStatus;
+            set
+            {
+                _connectionStatus = value;
+                RaisePropertyChangedEvent("ConnectionStatus");
             }
         }
 
@@ -79,12 +129,13 @@ namespace AscCutlistEditor.ViewModels.MQTT
             await _client.PublishAsync(message);
         }
 
+        // TODO: implementation is rather brittle, as it relies on Bryan's touchscreen feeding data.
+        // We might want to set up some way to mock sending messages to our client instead.
         public async Task StartClient()
         {
             // Create MQTT client.
             var options = new MqttClientOptionsBuilder()
                 .WithTcpServer("192.168.10.43", 1883)
-                //.WithTcpServer("192.168.0.164", 1883)
                 .Build();
 
             // Response to send on connection.
@@ -99,6 +150,8 @@ namespace AscCutlistEditor.ViewModels.MQTT
                 await _client.SubscribeAsync(_topic);
 
                 Debug.WriteLine("### SUBSCRIBED ###");
+
+                ConnectionStatus = "connected";
             });
 
             // Response to send on receiving a message.
@@ -149,15 +202,30 @@ namespace AscCutlistEditor.ViewModels.MQTT
 
         private void AddMachineData(dynamic data)
         {
-            MachineData machineData = new MachineData
+            // Run on UI thread.
+            // Select the correct dispatcher: if Application.Current is null,
+            // we're running unit tests and should use CurrentDispatcher instead.
+            Dispatcher dispatcher = Application.Current != null ?
+                Application.Current.Dispatcher :
+                Dispatcher.CurrentDispatcher;
+            dispatcher.Invoke(() =>
             {
-                ConnectionStatus = (string)data.SelectToken("connected"), // TODO: bool?
-                JobNumber = (string)data.SelectToken("tags.set1.MqttPub.JobNumber"),
-                LineStatus = (string)data.SelectToken("tags.set1.MqttPub.LineRunning"),
-                TimeStamp = (DateTime)data.SelectToken("timestamp")
-            };
+                DateTime timeStamp = (DateTime)data.SelectToken("timestamp");
 
-            MachineDataCollection.Add(machineData);
+                MachineData machineData = new MachineData
+                {
+                    ConnectionStatus = (string)data.SelectToken("connected"), // TODO: bool?
+                    JobNumber = (string)data.SelectToken("tags.set1.MqttPub.JobNumber"),
+                    LineStatus = (string)data.SelectToken("tags.set1.MqttPub.LineRunning"),
+                    TimeStamp = timeStamp
+                };
+
+                MachineDataCollection.Add(machineData);
+
+                double testValue = new Random().NextDouble() * 100;
+                _uptimeSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(timeStamp), testValue));
+                PlotModel.InvalidatePlot(true);
+            });
 
             // Update the latest if our data is newer.
             /*
--- ASC Cutlist Editor/ViewModels/MainViewModel.cs
+++ ASC Cutlist Editor/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using AscCutlistEditor.ViewModels.Cutlists;
 using AscCutlistEditor.ViewModels.FlatParts;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using AscCutlistEditor.Utility;
 using AscCutlistEditor.ViewModels.MQTT;
 using OxyPlot;
 using OxyPlot.Series;
@@ -17,9 +18,12 @@ namespace AscCutlistEditor.ViewModels
             new ObservableCollection<bool>(new[] { true, true, true });
 
         public CutlistImportViewModel CutlistViewModel { get; }
+
         public FlatPartRowsViewModel FlatPartRowsViewModel { get; }
 
         public MachineDataViewModel MachineDataViewModel { get; }
+
+        public MockMachineData MockMachineData { get; }
 
         public MainViewModel()
         {
@@ -29,36 +33,9 @@ namespace AscCutlistEditor.ViewModels
 
             MachineDataViewModel = new MachineDataViewModel();
 
-            // Test plot model
-            // Create the plot model
-            var tmp = new PlotModel { Title = "Simple example", Subtitle = "using OxyPlot" };
-
-            // Create two line series (markers are hidden by default)
-            var series1 = new LineSeries { Title = "Series 1", MarkerType = MarkerType.Circle };
-            series1.Points.Add(new DataPoint(0, 0));
-            series1.Points.Add(new DataPoint(10, 18));
-            series1.Points.Add(new DataPoint(20, 12));
-            series1.Points.Add(new DataPoint(30, 8));
-            series1.Points.Add(new DataPoint(40, 15));
-
-            var series2 = new LineSeries { Title = "Series 2", MarkerType = MarkerType.Square };
-            series2.Points.Add(new DataPoint(0, 4));
-            series2.Points.Add(new DataPoint(10, 12));
-            series2.Points.Add(new DataPoint(20, 16));
-            series2.Points.Add(new DataPoint(30, 25));
-            series2.Points.Add(new DataPoint(40, 5));
-
-            // Add the series to the plot model
-            tmp.Series.Add(series1);
-            tmp.Series.Add(series2);
-
-            // Axes are created automatically if they are not defined
-
-            // Set the Model property, the INotifyPropertyChanged event will make the WPF Plot control update its content
-            PlotModel = tmp;
+            MockMachineData = new MockMachineData();
+            MockMachineData.StartMockMessages();
         }
-
-        public PlotModel PlotModel { get; set; }
 
         /// <summary>
         /// Toggles the cutlist and its corresponding splitter's visibility.
