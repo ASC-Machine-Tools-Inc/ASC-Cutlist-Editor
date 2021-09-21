879cdb0c244e9bbd153df69ab1c693e5c4fc230e
Phi <pton@ascmt.com>
2021-09-21 13:42:36 -0700

    Working on queries

--- ASC Cutlist Editor/Utility/MQTT/Queries.cs
+++ ASC Cutlist Editor/Utility/MQTT/Queries.cs
@@ -14,14 +14,12 @@ namespace AscCutlistEditor.Utility.MQTT
     {
         private SqlConnection conn = new SqlConnection("TODO: connection string, store somewhere safe and let user set");
 
-        private IQueryable<OrderData> amsorder;
-
         /// <summary>
-        /// Select the order numbers, material type, and total run length of the orders for a specific machine.
+        /// Get the order numbers, material type, and total run length of the orders for a specific machine.
         /// </summary>
         /// <param name="machineId">The machine's corresponding id to retrieve orders for.</param>
         /// <returns>A list of the orders for a given machine.</returns>
-        public Task<IQueryable<dynamic>> getOrders(string machineId)
+        public IQueryable<dynamic> getOrders(string machineId)
         {
             return amsorder
                 .Select(x => new
@@ -33,6 +31,41 @@ namespace AscCutlistEditor.Utility.MQTT
                 })
                 .Where(x => x.orderno != null && x.machineNum.equals(machineId))
                 .GroupBy(x => new { x.orderno, x.material, x.machinenum, x.partno });
+        }
+
+        /// <summary>
+        /// Get the coil data for a specific coil.
+        /// </summary>
+        /// <param name="coilId">The coil's corresponding id to get data for.</param>
+        /// <returns>The data for that coil</returns>
+        public IQueryable<dynamic> getCoil(string coilId)
+        {
+            return amscoil
+                .Select(x => new
+                {
+                    x.startlength,
+                    x.lengthused,
+                    x.material
+                })
+                .Where(x => x.coilnumber.equals(coilId));
+        }
+
+        /// <summary>
+        /// Get the list of coils that aren't currently depleted for displaying on an HMI.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<dynamic> getCoils()
+        {
+            return amscoil
+                .Select(x => new
+                {
+                    x.coilnumber,
+                    x.description,
+                    x.material,
+                    x.startlength,
+                    x.lengthused
+                })
+                .Where(x => x.dateout == null);
         }
     }
 }
