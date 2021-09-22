bcd97b5961efb530005672c3067c92e2c99fbddc
Phi <pton@ascmt.com>
2021-09-22 10:36:52 -0700

    Query porting done until Bryan gets back

--- ASC Cutlist Editor/Utility/MQTT/Queries.cs
+++ ASC Cutlist Editor/Utility/MQTT/Queries.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.Globalization.DateTimeFormatting;
+using Windows.UI.Text;
 using AscCutlistEditor.Models;
 using AscCutlistEditor.Models.MQTT;
 
@@ -53,7 +54,7 @@ namespace AscCutlistEditor.Utility.MQTT
         /// <summary>
         /// Get the list of coils that aren't currently depleted for displaying on an HMI.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A list of non-depleted coils.</returns>
         public IQueryable<dynamic> getCoils()
         {
             return amscoil
@@ -66,6 +67,66 @@ namespace AscCutlistEditor.Utility.MQTT
                     x.lengthused
                 })
                 .Where(x => x.dateout == null);
+        }
+
+        /// <summary>
+        /// Get the list of orders for the given order number on the given machine.
+        /// </summary>
+        /// <param name="machineId">The machine's corresponding id to retrieve orders for.</param>
+        /// <param name="orderNum">The orders' corresponding id to retrieve orders for.</param>
+        /// <returns>A list of orders for the given machine and order number.</returns>
+        public IQueryable<dynamic> getCoils(string machineId, string orderNum)
+        {
+            return amsorder
+                .Select(x => new
+                {
+                    x.item_id,
+                    x.length,
+                    x.quantity,
+                    x.bundle
+                })
+                .Where(x => x.machinenum.equals(machineId) && x.orderno.equals(orderNum));
+        }
+
+        /// <summary>
+        /// Get the bundle data for a given order number.
+        /// </summary>
+        /// <param name="orderNum">The order number for the corresponding bundle.</param>
+        /// <returns>A list of distinct bundle data for the order number.</returns>
+        public IQueryable<dynamic> getBundle(string orderNum)
+        {
+            return amsbundle
+                .Select(x => new
+                {
+                    x.material,
+                    x.prodcode,
+                    x.user1,
+                    x.user2,
+                    x.user3,
+                    x.user4,
+                    x.custname,
+                    x.custaddr1,
+                    x.custaddr2,
+                    x.custcity,
+                    x.custstate,
+                    x.custzip
+                })
+                .Distinct()
+                .Where(x => x.orderno.equals(orderNum));
+        }
+
+        /// <summary>
+        /// Update amsproduct by inserting the contents of usageData.
+        /// </summary>
+        /// <param name="usageData">The data to add to amsproduct.</param>
+        private async void setUsageData(IQueryable<dynamic> usageData)
+        {
+            foreach (var Product in usageData)
+            {
+                // Something along these lines - need model for product, set up context
+                //_context.TimeLog.Add(TimeLog);
+                //await _context.SaveChangesAsync();
+            }
         }
     }
 }
