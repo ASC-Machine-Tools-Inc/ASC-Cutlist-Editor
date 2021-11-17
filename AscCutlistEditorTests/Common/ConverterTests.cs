cd0caf2d5516c296b9d6918cda7f3fc5311e914b
Phi <pton@uw.edu>
2021-11-17 09:34:59 -0800

    Added line running converter test

---
+++ AscCutlistEditorTests/Common/ConverterTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+using AscCutlistEditor.Utility.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AscCutlistEditorTests.Common
+{
+    [TestClass]
+    public class ConverterTests
+    {
+        [TestMethod]
+        public void BoolRadioConverterTest()
+        {
+            // Arrange
+            BoolRadioConverter converter = new BoolRadioConverter
+            {
+                Inverse = true
+            };
+
+            // Assert
+            Assert.IsFalse(BoolRadioConvertHelper(converter, true));
+            Assert.IsTrue(BoolRadioConvertHelper(converter, false));
+
+            Assert.IsFalse((bool)BoolRadioConvertBackHelper(converter, true));
+            // We expect null, since we only pay attention to selected radio buttons.
+            Assert.IsNull(BoolRadioConvertBackHelper(converter, false));
+
+            converter.Inverse = false;
+
+            Assert.IsTrue(BoolRadioConvertHelper(converter, true));
+            Assert.IsFalse(BoolRadioConvertHelper(converter, false));
+        }
+
+        private bool BoolRadioConvertHelper(BoolRadioConverter converter, bool value)
+        {
+            // ReSharper disable once PossibleNullReferenceException
+            return (bool)converter.Convert(value, typeof(bool), null, CultureInfo.CurrentCulture);
+        }
+
+        private object BoolRadioConvertBackHelper(BoolRadioConverter converter, bool value)
+        {
+            // ReSharper disable once PossibleNullReferenceException
+            return converter.ConvertBack(value, typeof(bool), null, CultureInfo.CurrentCulture);
+        }
+
+        [TestMethod]
+        public void LineRunningConverterTest()
+        {
+            // Arrange
+            LineRunningColorConverter converter = new LineRunningColorConverter();
+
+            // Assert
+            Assert.AreEqual(LineRunningConvertHelper(converter, "LINE RUNNING"), Brushes.Green);
+            Assert.AreEqual(LineRunningConvertHelper(converter, "LINE STOPPED"), Brushes.Red);
+            Assert.AreEqual(LineRunningConvertHelper(converter, "INVALID VALUE"), Brushes.Transparent);
+        }
+
+        private Brush LineRunningConvertHelper(LineRunningColorConverter converter, string value)
+        {
+            // ReSharper disable once PossibleNullReferenceException
+            return (Brush)converter.Convert(value, typeof(Brush), null, CultureInfo.CurrentCulture);
+        }
+    }
+}
